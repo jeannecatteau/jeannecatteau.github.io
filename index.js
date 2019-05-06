cdc5c5f3575bcff66c49854e246c34276d87bcba
Stephane Ruhlmann <stephanej.ruhlmann@yahoo.com>
2019-05-05 21:29:52 -0400

    Remove console.log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function toggleNavMenu() {
   var navbar = document.getElementsByClassName("navbar")[0];
-  console.log("navbar:", navbar.style.visibility);
   navbar.style.visibility !== "visible"
     ? (navbar.style.visibility = "visible")
     : (navbar.style.visibility = "hidden");
